22695117b7b3327cfc1d41755fff090bc325737e
MohamedSamyHossebo <eng.mohamedsamyhossebo@gmail.com>
2024-12-04 01:40:23 +0200

    setup the main structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './products/components/home/home.component';
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    //TODO doing routes
+    { path: "**", redirectTo: "home", pathMatch: "full" }
+];
